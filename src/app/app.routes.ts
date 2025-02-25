fe1133a9688e473251d64a178e57c832ba72383d
NoeliaRominaAcosta <noeliaromina20@gmail.com>
2025-02-25 00:23:08 -0300

    Initial commit

---
+++ src/app/app.routes.ts
@@ -0,0 +1,12 @@
+import { Routes } from "@angular/router";
+export const routes: Routes = [
+  {
+    path: 'dashboard',
+    loadComponent: () =>
+      import('./gifs/pages/dashboard-page/dashboard-page.component'),
+  },
+  {
+    path: '**',
+    redirectTo: 'dashboard',
+  },
+];
